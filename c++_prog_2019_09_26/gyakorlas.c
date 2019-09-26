20594e8fdc69d4e4f89f93bf6c9c3d0b6ae91bc5
NZsolt98 <43852977+NZsolt98@users.noreply.github.com>
2019-09-26 09:30:00 +0200

    Gyakorlo program feltöltese

---
+++ c++_prog_2019_09_26/gyakorlas.c
@@ -0,0 +1,19 @@
+#include <iomanip>
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello">>endl;
+    double a=9.315726;
+    cout<< setw(5)<<setprecision(2)<<a<<endl;
+    cout<<fixed<<setprecision(2)<<a<<endl;
+
+    int a2;
+    cout<<"b értéke?";
+    cin>>b;
+    cout<<endl<<b<<endl;
+}
+
